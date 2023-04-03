8a8a9783d58429c6f8dcf4fde3f062bf0d2c6282
Daniel Veiga <daveigantu@gmail.com>
2023-04-03 17:31:24 -0300

    Ajuste de bug na aplicação do padding

--- aes-128-pcbc-iso-iec.c
+++ aes-128-pcbc-iso-iec.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <math.h>
+#include <termios.h>
 #include "aes.h"
 
 #define SIZE_BLOCK 16
@@ -29,14 +29,12 @@ void xor_block(uint8_t * block1, uint8_t * block2, uint8_t * output){
         output[i] = block1[i] ^ block2[i];
 }
 
-int padding(uint8_t * block, int size){
+void padding(uint8_t * block, long size){
     if (size < SIZE_BLOCK){
         block[size] = 0x80;
         for (int i = size + 1; i < SIZE_BLOCK ; i++)
             block[i] = 0x00;
-        return false;
     }
-    return true;
 }
 
 void encript_file(uint8_t * key, char * file_name_in, char * file_name_out){
@@ -45,23 +43,23 @@ void encript_file(uint8_t * key, char * file_name_in, char * file_name_out){
     FILE * file_out = fopen(file_name_out, "wb");
 
     fseek(file_in, 0L, SEEK_END);
-    long sizeFileIn = ftell(file_in);
+    long nBytesToRead = ftell(file_in);
     fseek(file_in, 0L, SEEK_SET);
 
-    long nBytesRead = 0;
     int addBlockPadding = false;
 
     uint8_t IV[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    while (sizeFileIn > nBytesRead || addBlockPadding){
+    while (nBytesToRead > 0 || addBlockPadding){
 
         uint8_t block_plaintext[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
         if (!addBlockPadding){
-            nBytesRead += fread(block_plaintext, 1, SIZE_BLOCK, file_in);
+            long nBytesRead = fread(block_plaintext, 1, SIZE_BLOCK, file_in);
+            nBytesToRead -= nBytesRead;
             if (nBytesRead < SIZE_BLOCK)
                 padding(block_plaintext, nBytesRead);
-            else if ((sizeFileIn - nBytesRead) == 0)
+            else if (nBytesToRead == 0)
                 addBlockPadding = true;
         }
         else {
@@ -86,18 +84,17 @@ void decript_file(uint8_t * key, char * file_name_in, char * file_name_out){
     FILE * file_out = fopen(file_name_out, "wb");
 
     fseek(file_in, 0L, SEEK_END);
-    long sizeFileIn = ftell(file_in);
+    long nBytesToRead = ftell(file_in);
     fseek(file_in, 0L, SEEK_SET);
-
-    long nBytesRead = 0;
 
     uint8_t IV[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    while (sizeFileIn > nBytesRead){
+    while (nBytesToRead > 0){
 
         uint8_t block_ciphertext[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         
-        nBytesRead += fread(block_ciphertext, 1, SIZE_BLOCK, file_in);
+        long nBytesRead = fread(block_ciphertext, 1, SIZE_BLOCK, file_in);
+        nBytesToRead -= nBytesRead;
 
         uint8_t iv_xor_pt[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         AES128_Decrypt(block_ciphertext, key, iv_xor_pt);
@@ -105,7 +102,7 @@ void decript_file(uint8_t * key, char * file_name_in, char * file_name_out){
         uint8_t block_plaintext[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         xor_block(iv_xor_pt, IV, block_plaintext);
 
-        if ((sizeFileIn - nBytesRead) == 0){
+        if (nBytesToRead == 0){
             for (int i = SIZE_BLOCK - 1; i >= 0; i --)
                 if (block_plaintext[i] == 0x80 && i != 0)
                     fwrite(block_plaintext, i, 1, file_out);
@@ -117,28 +114,79 @@ void decript_file(uint8_t * key, char * file_name_in, char * file_name_out){
     }
 }
 
+ssize_t my_getpass(char **lineptr, size_t *n, FILE *stream)
+{
+  struct termios old, new;
+  int nread;
+
+  /* Turn echoing off and fail if we can’t. */
+  if (tcgetattr (fileno (stream), &old) != 0)
+    return -1;
+  new = old;
+  new.c_lflag &= ~ECHO;
+  if (tcsetattr (fileno (stream), TCSAFLUSH, &new) != 0)
+    return -1;
+
+  /* Read the passphrase */
+  nread = getline(lineptr, n, stream);
+
+  /* Restore terminal. */
+  (void) tcsetattr(fileno (stream), TCSAFLUSH, &old);
+
+  return nread;
+}
+
 int main (int argc, char *argv[]){
 
-    char * action = argv[1];
-    char * file_name_in = argv[2];
-    char * file_name_out = argv[3];
+    char * action;
+    char * file_name_in;
+    char * file_name_out;
+    size_t sizeKey = SIZE_BLOCK;
 
-    uint8_t key[SIZE_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    uint8_t * key = (uint8_t * ) calloc(sizeKey, sizeof(uint8_t));
 
+    if (argc < 3){
+        printf("ERRO: Quantidade de argumentos insuficiente!\n");
+        return 1;
+    }
+    else {
+        action = argv[1];
+        file_name_in = argv[2];
+        
+        if (argc > 3){
+            if (!strcmp(argv[3], "-o") && argc == 5)
+                file_name_out = argv[4];
+            else {
+                printf("ERRO: Comando inválido ou quantidade de argumentos insuficiente!\n");
+                return 1;
+            }
+        }
+        else{
+            char file_out_gerate [sizeof(file_name_in) + sizeof(action) + 1];
+            strcpy(file_out_gerate, file_name_in);
+            strcat(file_out_gerate, ".");
+            strcat(file_out_gerate, action);
+
+            file_name_out = file_out_gerate;
+        }
+    }
+    
     if (!file_exist(file_name_in)){
         printf("ERRO: Arquivo %s não foi encontrado!\n", file_name_in);
         return 1;
     }
 
     printf("Insira sua chave: ");
-    fgets((char *) key, SIZE_BLOCK, stdin );
+    my_getpass((char **) &key, &sizeKey, stdin);
+    printf("\n");
+    //fgets((char *) key, SIZE_BLOCK, stdin );
 
     if (!strcmp(action, ENCRIPT))
         encript_file(key, file_name_in, file_name_out);
     else if (!strcmp(action, DECRIPT))
         decript_file(key, file_name_in, file_name_out);
     else {
-        printf("Tipo de ação inválida\n");
+        printf("ERRO: Tipo de ação inválida\n");
         return 1;
     }
 
